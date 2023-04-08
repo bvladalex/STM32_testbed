a7679fb517de78e1aa5791b741d4dabb3b54f0cc
Vlad Barbalata <bvladalex6@gmail.com>
2023-04-08 13:58:32 +0300

    changed fullscale value to 100%

--- spi_gpio_int/Debug/startup/subdir.mk
+++ spi_gpio_int/Debug/startup/subdir.mk
@@ -22,7 +22,7 @@ startup/%.o: ../startup/%.s
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Assembler'
 	@echo $(PWD)
-	arm-none-eabi-as -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -I"D:/stm_workspace/spi_gpio_int/inc" -g -o "$@" "$<"
+	arm-none-eabi-as -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -I"/Volumes/other_stuff/electro_stuff/STM32_testbed/spi_gpio_int/inc" -g -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
@@ -30,7 +30,7 @@ startup/%.o: ../startup/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DUSE_STDPERIPH_DRIVER -I"D:/stm_workspace/spi_gpio_int/inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DUSE_STDPERIPH_DRIVER -I"/Volumes/other_stuff/electro_stuff/STM32_testbed/spi_gpio_int/inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
